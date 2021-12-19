0286d0a2df323b2680f4f3ce83f17735fab034da
gutosenna <79454869+gutosenna@users.noreply.github.com>
2021-12-19 13:25:55 -0300

    Add files via upload

---
+++ Lua/Code/ex1.lua
@@ -0,0 +1,3 @@
+a = 2 + 3
+
+print("O resultado eh " .. tostring(a))
---
+++ Lua/Code/ex2.lua
@@ -0,0 +1,34 @@
+a = 3 -- number
+b = "agua" -- string
+x = true -- boolean
+-- nil -- Not in list(NULL)
+-- number: Numérico(Real)
+address = 0x0000aaf8 -- hexadecimal
+
+-- string: Textos
+
+-- table: Tabelas -> Tipo de dados estruturado
+pessoa = {}
+pessoa.nome = "Guto"
+pessoa.idade = 30
+
+pessoa =
+{
+	nome = "Guto",
+	idade = "26"
+}
+
+pares = {0,2,4,6,8,10} -- tabela array
+
+-- function: Funções
+function soma(a,b)
+	return a + b
+end
+
+print(soma(1,2))
+
+-- thread: Corrotinas
+
+-- userdata: Tipos customizados definidos pelo usuário(em C)
+
+
